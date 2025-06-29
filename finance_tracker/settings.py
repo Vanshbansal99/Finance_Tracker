65752a5d9f019fc796d155786a81801bd463dd5a
white <vbvansh99@gmail.com>
2025-06-29 22:56:47 +0530

    second committ

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lob8d_xm73&0hwtmssag5v94h=$wyb69j+9q%t6z738dxc*qd9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['finance-tracker-vs7l.onrender.com']
 
 
 # Application definition
